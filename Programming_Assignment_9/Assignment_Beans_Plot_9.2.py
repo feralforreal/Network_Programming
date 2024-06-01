a5c9331a09780fe545a3d81fc037d9ef941beca2
Srivaish <srgo6857@colorado.edu>
2024-06-01 00:10:56 -0600

    pushing files and code

--- Programming_Assignment_9/Assignment_Beans_Plot_9.2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import plotly.express as px
-Col_Names = ["Price", "Date", "Time"]
-df =pd.read_csv("C:/Users/Srivaishnavi/PycharmProjects/pythonProject/Beans_Lab9_Assignment_9.csv", names=Col_Names)
-df=df.sort_values(by=['Price'], ascending=True)
-fig = px.line(df, x='Time', y='Price', title = 'Price vs Time for the beans')
-fig.show()
-
-
